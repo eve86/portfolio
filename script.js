e1c1211336af5e6ae0eaeddf3b5369e60bc32c45
eve <46179055+eve86@users.noreply.github.com>
2020-05-04 21:34:47 +0900

    Update script.js

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ $(window).scroll(function () {
     var elemPos = $(this).offset().top,
       scroll = $(window).scrollTop(),
       windowHeight = $(window).height();
-    if (scroll > elemPos - windowHeight + 300) {
+    if (scroll > elemPos - windowHeight + 200) {
       $(this).addClass('service-item-show');
     }
   });
@@ -72,7 +72,7 @@ $(window).scroll(function () {
     var elemPos = $(this).offset().top,
       scroll = $(window).scrollTop(),
       windowHeight = $(window).height();
-    if (scroll > elemPos - windowHeight + 300) {
+    if (scroll > elemPos - windowHeight + 200) {
       $(this).addClass('skill-item-show');
     }
   });
